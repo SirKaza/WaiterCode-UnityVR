32d90f408b0f1f5f5b10c8d35cd8706ad6a800e3
Marc Casanova <mcasanto14@alumnes.ub.edu>
2023-03-30 07:27:47 +0200

    without backend v2

--- TFG-VR/Assets/Scripts/menuIniciScript.cs
+++ TFG-VR/Assets/Scripts/menuIniciScript.cs
@@ -136,33 +136,122 @@ public class menuIniciScript : MonoBehaviour
     }
 
     IEnumerator searchUser(){
-        var name = GameObject.Find("UserNameFieldLogIn").GetComponent<TMPro.TMP_InputField>().text;
-        var password = GameObject.Find("PasswordFieldLogIn").GetComponent<TMPro.TMP_InputField>().text;
-        //var uri = "mongodb + srv://alexfuentesraventos:Baloncesto8@cluster-qbp6st97.lg4ch.mongodb.net/test/searchUser/" + name + "&" + password;
-        var uri = "https://shrouded-sands-87010.herokuapp.com/searchUser/"+name+"&"+password;
-
-        using (UnityWebRequest request = UnityWebRequest.Get(uri))
-        {
-            yield return request.SendWebRequest();
+        // var name = GameObject.Find("UserNameFieldLogIn").GetComponent<TMPro.TMP_InputField>().text;
+        // var password = GameObject.Find("PasswordFieldLogIn").GetComponent<TMPro.TMP_InputField>().text;
+        // //var uri = "mongodb + srv://alexfuentesraventos:Baloncesto8@cluster-qbp6st97.lg4ch.mongodb.net/test/searchUser/" + name + "&" + password;
+        // //var uri = "https://shrouded-sands-87010.herokuapp.com/searchUser/"+name+"&"+password;
+        
+        //var uri = "mongodb://localhost/TFG2023/searchUser/"+name+"&"+password;
+        // using (UnityWebRequest request = UnityWebRequest.Get(uri))
+        // {
+        //     yield return request.SendWebRequest();
  
-            if (request.isNetworkError) // Error
-            {
-                GameObject.Find("TituloLogIn").GetComponent<TMPro.TextMeshProUGUI>().text = request.error;
-                Debug.LogError(request.error);
-                GameObject.Find("PasswordFieldLogIn").SetActive(false);
-                GameObject.Find("UserNameFieldLogIn").SetActive(false);
-            }
-            else // Success
-            {
-                var current_user = JsonUtility.FromJson<UserJSON>(request.downloadHandler.text);
-                userControl.setCurrentUser(current_user);
-                GameObject.Find("TituloLogIn").GetComponent<TMPro.TextMeshProUGUI>().text = userControl.getCurrentUser().name;
+        //     if (request.isNetworkError) // Error
+        //     {
+        //         GameObject.Find("TituloLogIn").GetComponent<TMPro.TextMeshProUGUI>().text = request.error;
+        //         Debug.LogError(request.error);
+        //         GameObject.Find("PasswordFieldLogIn").SetActive(false);
+        //         GameObject.Find("UserNameFieldLogIn").SetActive(false);
+        //     }
+        //     else // Success
+        //     {
+        //         var current_user = JsonUtility.FromJson<UserJSON>(request.downloadHandler.text);
+        //         userControl.setCurrentUser(current_user);
+        //         GameObject.Find("TituloLogIn").GetComponent<TMPro.TextMeshProUGUI>().text = userControl.getCurrentUser().name;
                 
-                showGameModeMenu();
+        //         showGameModeMenu();
 
-            }
-        }
+        //     }
+        // }
+        UserJSON current_user = new UserJSON();
+        current_user.name = "AutoName";
+        current_user.password = "123";
+        current_user.currentDay = 2;
+        current_user.tutorialChefCompleted = false;
+        current_user.tutorialWaiterCompleted = false;
+        current_user.volume = "1.0";
+        current_user.dataCollection = "Start data collection";
+
+        DataLevel dataLevel = new DataLevel();
+        dataLevel.totalExp = 0;
+        dataLevel.chefExp = 0;
+        dataLevel.waiterExp = 0;
+        dataLevel.waiterLevel = 1;
+        dataLevel.chefLevel = 1;
+        dataLevel.basicOrdersChef = 0;
+        dataLevel.conditionalIfOrdersChef = 0;
+        dataLevel.conditionalIfElseOrdersChef = 0;
+        dataLevel.iterativeOrdersChef = 0;
+        dataLevel.basicOrdersWaiter = 0;
+        dataLevel.conditionalIfOrdersWaiter = 0;
+        dataLevel.conditionalIfElseOrdersWaiter = 0;
+        dataLevel.iterativeOrdersWaiter = 0;
+        current_user.dataLevel = dataLevel;
+
+        AchievementsJSON achievements = new AchievementsJSON();
+        achievements.firstOrderChef = false;
+        achievements.tenOrdersChef = false;
+        achievements.twentyfiveOrdersChef = false;
+        achievements.fiftyOrdersChef = false;
+        achievements.hundredOrdersChef = false;
+
+        achievements.firstBasicOrderChef = false;
+        achievements.tenBasicOrdersChef = false;
+        achievements.twentyfiveBasicOrdersChef = false;
+        achievements.fiftyBasicOrdersChef = false;
+
+        achievements.firstConditionalOrderChef = false;
+        achievements.tenConditionalOrdersChef = false;
+        achievements.twentyfiveConditionalOrdersChef = false;
+        achievements.fiftyConditionalOrdersChef = false;
+
+        achievements.firstIterativeOrderChef = false;
+        achievements.tenIterativeOrdersChef = false;
+        achievements.thirtyIterativeOrdersChef = false;
+
+        achievements.firstConditionalIfElseOrderChef = false;
+        achievements.tenConditionalIfElseOrdersChef = false;
+        achievements.thirtyConditionalIfElseOrdersChef = false;
+
+        achievements.firstConditionalIfElseOrderChef = false;
+        achievements.tenConditionalIfElseOrdersChef = false;
+        achievements.thirtyConditionalIfElseOrdersChef = false;
+
+        achievements.firstOrderWaiter = false;
+        achievements.tenOrdersWaiter = false;
+        achievements.twentyfiveOrdersWaiter = false;
+        achievements.fiftyOrdersWaiter = false;
+        achievements.hundredOrdersWaiter = false;
+
+        achievements.firstBasicOrderWaiter = false;
+        achievements.tenBasicOrdersWaiter = false;
+        achievements.twentyfiveBasicOrdersWaiter = false;
+        achievements.fiftyBasicOrdersWaiter = false;
+
+        achievements.firstConditionalOrderWaiter = false;
+        achievements.tenConditionalOrdersWaiter = false;
+        achievements.thirtyConditionalOrdersWaiter = false;
+        achievements.fiftyConditionalOrdersWaiter = false;
+
+        achievements.firstConditionalIfOrderWaiter = false;
+        achievements.tenConditionalIfOrdersWaiter = false;
+        achievements.thirtyConditionalIfOrdersWaiter = false;
+
+        achievements.firstConditionalIfElseOrderWaiter = false;
+        achievements.tenConditionalIfElseOrdersWaiter = false;
+        achievements.thirtyConditionalIfElseOrdersWaiter = false
+        ;
+        achievements.firstIterativeOrderWaiter = false;
+        achievements.tenIterativeOrdersWaiter = false;
+        achievements.thirtyIterativeOrdersWaiter = false;
+        current_user.achievements = achievements;
+
+        userControl.setCurrentUser(current_user);
+        GameObject.Find("TituloLogIn").GetComponent<TMPro.TextMeshProUGUI>().text = userControl.getCurrentUser().name;
+        showGameModeMenu();
+        yield return null;
     }
+    
     public void SignIn(){
         StartCoroutine(createUser());
     }
@@ -170,7 +259,8 @@ public class menuIniciScript : MonoBehaviour
     IEnumerator createUser(){
         var name = GameObject.Find("UserNameFieldRegister").GetComponent<TMPro.TMP_InputField>().text;
         var password = GameObject.Find("PasswordFieldRegister").GetComponent<TMPro.TMP_InputField>().text;
-        var uri = "https://shrouded-sands-87010.herokuapp.com/createUser/"+name+"&"+password;
+        //var uri = "https://shrouded-sands-87010.herokuapp.com/createUser/"+name+"&"+password;
+        var uri = "mongodb://localhost/TFG2023/createUser/"+name+"&"+password;
 
         using (UnityWebRequest request = UnityWebRequest.Get(uri))
         {
@@ -199,9 +289,11 @@ public class menuIniciScript : MonoBehaviour
         GameObject.Find("UserControl").GetComponent<UserController>().setCurrentDay(100000);
         string jsonData = JsonUtility.ToJson(GameObject.Find("UserControl").GetComponent<UserController>().getCurrentUser());
         //var jsonString = JsonUtility.ToJson(jsonData) ?? "";
-       
+        
         //byte[] myData = System.Text.Encoding.UTF8.GetBytes();
-        UnityWebRequest www = UnityWebRequest.Put("http://shrouded-sands-87010.herokuapp.com/sendUser", jsonData);
+
+        //"http://shrouded-sands-87010.herokuapp.com/sendUser"
+        UnityWebRequest www = UnityWebRequest.Put("mongodb://localhost/TFG2023/sendUser/", jsonData);
         www.SetRequestHeader("Content-Type", "application/json");
 
         yield return www.SendWebRequest();
